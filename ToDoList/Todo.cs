5cad1c9121163b4b076233853e8585b53cd5e1ab
eduardo-pitanga <edudupilo@gmail.com>
2024-10-22 21:08:41 -0300

    ToDoList

---
+++ ToDoList/Todo.cs
@@ -0,0 +1,8 @@
+namespace ToDoList;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ ToDoList/TodoBd.cs
@@ -0,0 +1,10 @@
+namespace ToDoList;
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
